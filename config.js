777ccd0b558b837a1ab4957a5858d86f4f66db6c
Alexander Salas Bastidas <asalas@teclib.com>
2016-12-19 23:51:22 +0100

    chore(gh-page): Deploy of the version 1.0.0

---
+++ config.js
@@ -0,0 +1,3 @@
+/* Copyright by Teclib' @2016 */
+angular.module('FlyveMDM').constant('GLPI_API_URL', 'https://stork-mdm.com/glpi/apirest.php').constant('USER_TOKEN', 'q56hqkniwot8wntb3z1qarka5atf365taaa2uyjrn').constant('DEBUG', false).constant('BUILD_ID', 0);
+//angular.module('FlyveMDM').constant('GLPI_API_URL', 'https://stork.teclib.net/glpi/apirest.php').constant('USER_TOKEN', 'q7yv8z2ryu2sxnwrk5mxx9zw8y0l8w6y44ta4v33q').constant('DEBUG', false).constant('BUILD_ID', 0);
